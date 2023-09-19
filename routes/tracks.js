9e33d432334bbdb4a1318b0c88449222481dcc0b
AntonioAnchundia <antonio-alex13@outlook.com>
2023-09-19 16:36:03 -0500

    cambios2

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { validatorCreateItem } = require("../validators/tracks");
 const {getItems, getItem, createItem, updateItem, deleteItem} = require('../controllers/tracks')
 
 router.get("/", getItems)
 router.get(":id", getItem)
-router.post("/", createItem)
+router.post("/", validatorCreateItem ,createItem)
 router.put("/", updateItem)
 router.delete("/", deleteItem)
 
