c4e71a454c510c3cf81674956d3f137d7c5c5f56
AntonioAnchundia <antonio-alex13@outlook.com>
2023-09-20 16:57:41 -0500

    ultimos cambios

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { validatorCreateItem } = require("../validators/tracks");
+const customHeader = require("../middleware/customHeader")
 const {getItems, getItem, createItem, updateItem, deleteItem} = require('../controllers/tracks')
 
 router.get("/", getItems)
 router.get(":id", getItem)
-router.post("/", validatorCreateItem ,createItem)
+router.post("/", validatorCreateItem, customHeader ,createItem)
 router.put("/", updateItem)
 router.delete("/", deleteItem)
 
